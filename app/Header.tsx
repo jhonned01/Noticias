b9c0d18841bb6453563767ef98926cd1b7694eaf
jhonned01 <jhonned01@gmail.com>
2023-02-27 16:08:51 -0500

    vamos señor

--- app/Header.tsx
+++ app/Header.tsx
@@ -8,7 +8,7 @@ const Header = () => {
       <div className="grid grid-cols-3 p-10 items-center">
         <Bars3Icon className="w-8 h-8 cursor-pointer" />
         <Link href="/" prefetch={false}>
-          <h1 className="font-serif text-4xl text-center">
+          <h1 className="font-serif text-4xl text-center ">
             The{" "}
             <span className="underline decoration-6 decoration-orange-400">
               Jhonned01
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,15 @@
 import React from "react";
 
 const HomePage = () => {
-  return <div>HomePage</div>;
+  return (
+    <div className=" bg-red-600 max-h-32 w-full  animate-bounce">
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum, dicta
+      voluptatum vel dolor, obcaecati nulla voluptatibus laboriosam quia quam ab
+      excepturi distinctio iusto tenetur. Officia animi eius a nulla maiores?
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestias labore
+      r ipsam, quaerat esse dolores nemo libero dolore ratione! Pariatur?
+    </div>
+  );
 };
 
 export default HomePage;
